8bb2dbfbf1beb8645969637e52c0c316498ce16d
Shahrukh <shaizykhan282@gmail.com>
2020-12-09 21:35:43 +0530

    Initial

--- app/src/main/java/com/example/tutorshubaligarh/MainActivity.java
+++ app/src/main/java/com/example/tutorshubaligarh/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tutorshubaligarh;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -7,12 +8,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     CardView cardView;
     CardView cardView1;
+    AlertDialog dialogBuilder;
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LayoutInflater inflater = this.getLayoutInflater();
         cardView = findViewById(R.id.cardView);
         cardView1 = findViewById(R.id.cardView1);
 
@@ -29,8 +34,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(isNetworkConnected()){
-                    Intent intent= new Intent(MainActivity.this, SearchActivity.class);
-                    startActivity(intent);
+                    //Intent intent= new Intent(MainActivity.this, SearchActivity.class);
+                    //startActivity(intent);
+
+                    dialogBuilder = new AlertDialog.Builder(MainActivity.this).create();
+                    View dialogView = inflater.inflate(R.layout.dialogbox, null);
+
+                    EditText editText1 =dialogView.findViewById(R.id.search_class);
+                    EditText editText2 =dialogView.findViewById(R.id.seacrh_city);
+                    //EditText editText3 =dialogView.findViewById(R.id.dialog_title);
+                    Button searchButton =dialogView.findViewById(R.id.search_button);
+                    searchButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(MainActivity.this, "Submit", Toast.LENGTH_SHORT).show();
+                            dialogBuilder.dismiss();
+                        }
+                    });
+                    dialogBuilder.setView(dialogView);
+                    dialogBuilder.show();
+
+
                 }
                 else{
                     Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
